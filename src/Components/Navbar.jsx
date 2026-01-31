bf05636bda4b1f9df174a2ed3b6ffe8f19a7ea75
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-01-31 19:36:19 +0530

    nabar and other initial setup

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+function Navbar() {
+  return (
+    
+      <div className='max-w-screen-xl h-4 bg-white '></div>
+    
+  )
+}
+
+export default Navbar
