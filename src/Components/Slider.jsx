23b4350a65ad3fd5091121abdf5ba208a186a3c1
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-02-15 16:42:09 +0530

    slider animation

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,10 +1,15 @@
+import {motion} from 'framer-motion'
 import React from 'react'
 
 function Slider({url}) {
   return (
-    <div className='flex w-full py-8 gap-20 whitespace-nowrap '>
-      {url.map((img,index)=><img src={img} className='w-[7vw] flex-shrink-0' /> )}
-      {url.map((img,index)=><img src={img} className='w-[7vw] flex-shrink-0'/> )}
+    <div className=" flex w-full overflow-hidden">
+      <motion.div initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat:Infinity,ease:'linear',duration:'15'}} className='flex w-full shrink-0 py-10 gap-40 whitespace-nowrap '>
+        {url.map((img,index)=><img src={img} className='w-[7vw] flex-shrink-0'/> )}
+      </motion.div>
+      <motion.div initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat:Infinity,ease:'linear',duration:'15'}} className='flex w-full shrink-0 py-10 gap-40 whitespace-nowrap '>
+        {url.map((img,index)=><img src={img} className='w-[7vw] flex-shrink-0'/> )}
+      </motion.div>
     </div>
   )
 }
