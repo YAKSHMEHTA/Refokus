a839ec8ebb46cdc93e202f0a5ac1c678704a6a37
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-03-11 19:31:28 +0530

    image fix

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -34,6 +34,45 @@ function Products() {
     },
   ];
 
+  var image = [
+    {
+      url: "https://i.pinimg.com/736x/dc/d2/02/dcd20235abccefe2a02ab7ea37b5ac0c.jpg",
+      top: "50%",
+      left: "50%",
+      isActive: true,
+    },
+    {
+      url: "https://i.pinimg.com/736x/61/a8/d9/61a8d9a75e2a41c4eb4ae5f7466da80d.jpg",
+      top: "56%",
+      left: "44%",
+      isActive: false,
+    },
+    {
+      url: "https://i.pinimg.com/736x/d0/5e/87/d05e8716446c82a1e178707ac3476284.jpg",
+      top: "45%",
+      left: "56%",
+      isActive: false,
+    },
+    {
+      url: "https://i.pinimg.com/1200x/d7/25/1e/d7251e4b6a86143787b4b76afe48ded2.jpg",
+      top: "60%",
+      left: "53%",
+      isActive: false,
+    },
+    {
+      url: "https://i.pinimg.com/736x/a6/db/73/a6db736c419380d2c6102fa8dc2d2d35.jpg",
+      top: "43%",
+      left: "40%",
+      isActive: false,
+    },
+    {
+      url: "https://i.pinimg.com/736x/91/c4/ee/91c4ee1ca668994679f7ea8742856aba.jpg",
+      top: "60%",
+      left: "55%",
+      isActive: false,
+    },
+  ]
+
   const [pos, setPos] = useState(0);
 
   const mover = (value) => {
@@ -56,22 +95,22 @@ function Products() {
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             className="w-full  h-full bg-blue-400"
-          ></motion.div>
+          ><img src={image[0].url} alt="" /></motion.div>
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             className="w-full  h-full bg-blue-400"
-          ></motion.div>
+          ><img src={image[1].url} alt="" /></motion.div>
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             className="w-full  h-full bg-blue-400"
-          ></motion.div>
+          ><img src={image[2].url} alt="" /></motion.div>
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             className="w-full  h-full bg-blue-400"
-          ></motion.div>
+          ><img src={image[3].url} alt="" /></motion.div>
         </motion.div>
       </div>
     </div>
